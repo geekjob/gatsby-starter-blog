ae947863b23c5c415cc73ad900825cbb38cc94b1
geekjob <47718537+geekjob@users.noreply.github.com>
2022-01-11 19:23:44 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -24,7 +24,7 @@ const Layout = ({ location, title, children }) => {
     <div className="global-wrapper" data-is-root-path={isRootPath}>
       <header className="global-header">{header}</header>
       <main>{children}</main>
-      <footer>
+      <footer style="font-size:10px">
         © {new Date().getFullYear()}, Built with
         {` `}
         <a href="https://www.gatsbyjs.com">Gatsby</a>
